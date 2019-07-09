753d7e316901a89edfd12f151d22de4c3b5f1c12
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-08 21:20:34 -0400

    trying to get score with an answer array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,8 +7,11 @@ function myTimer() {
   if (timelimit > 0) {
     curmin=Math.floor(timelimit/60);
     cursec=timelimit%60;
-    if (curmin!=0) { curtime=curmin+" minutes and "+cursec+" seconds left"; }
-              else { curtime=cursec+" seconds left"; }
+    if (curmin!=0) { 
+      curtime=curmin+" minutes and "+cursec+" seconds left"; 
+    }else { 
+      curtime=cursec+" seconds left"; 
+    }
     $('timeleft').innerHTML = curtime;
   } else {
     $('timeleft').innerHTML = timelimit+' - Out of Time - Impeachment!!!';
@@ -16,7 +19,7 @@ function myTimer() {
   }
   timelimit--;
 }
-
+//golbal variables
 let pos = 0;
 let posNum; 
 let choice; 
@@ -24,7 +27,7 @@ let correct = 0;
 let score = 0;
 let maxtimelimit = 20;
 let timelimit = maxtimelimit;  
-
+//question array
 let questions = [
   ["Who was the only President to serve more than two terms?", "Ulysses S. Grant", "Theodore Roosevelt","Franklin D. Roosevelt","George Washington","Franklin D. Roosevelt"],
     ["Who was the only President to serve two non-consecutive terms?", "Theodore Roosevelt", "Grover Cleveland", "Ronald Regan", "Woodrow Wilson", "Grover Clevland"],
@@ -39,11 +42,14 @@ let questions = [
   
    
 ];
+let answers = []
 //chose a new question in array
 let questionOrder = [];
 function setQuestionOrder() {
  questionOrder.length = 0;
- for (var i=0; i<questions.length; i++) { questionOrder.push(i); }
+ for (var i=0; i<questions.length; i++) { 
+   questionOrder.push(i);
+ }
  questionOrder.sort(randOrd);  
  pos = 0;  
  posNum = questionOrder[pos];
@@ -69,12 +75,12 @@ function renderQuestion() {
   let choiceA = questions[posNum][1];
   let choiceB = questions[posNum][2];
   let choiceC = questions[posNum][3];
-  let choiceD = questions[posNum][4];
+  let choiceD = questions[posNum][4]; 
   test.innerHTML = "<h3>"+question+"</h3>";
   test.innerHTML += "<label><input type='radio' name='choices' value='A'> "+choiceA+"</label><br>";
   test.innerHTML += "<label><input type='radio' name='choices' value='B'> "+choiceB+"</label><br>";
   test.innerHTML += "<label><input type='radio' name='choices' value='C'> "+choiceC+"</label><br>";
-  test.innerHTML += "<label><input type='radio' name='choices' value='d'> "+choiceD+"</label><br><br>";
+  test.innerHTML += "<label><input type='radio' name='choices' value='d'> "+choiceD+"</label><br>";
   test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
   timelimit = maxtimelimit;
   clearInterval(myVar);
@@ -83,13 +89,20 @@ function renderQuestion() {
 //check to see if answers are correct
 function checkAnswer(){
   let choices = document.getElementsByName("choices");
+  console.log(choices);
   for (var i=0; i<choices.length; i++) {
     if (choices[i].checked) { choice = choices[i].value; }
   }
   score++;
-  if (choice == questions[posNum][5] && timelimit > 0) { correct++; }
-  pos++;  posNum = questionOrder[pos];
-  if (pos < questions.length) { renderQuestion(); } else { renderResults(); }
+  if (choice == [posNum][5] && timelimit > 0) {
+     correct++; }
+  pos++;  
+  posNum = questionOrder[pos];
+  if (pos < questions.length) {
+     renderQuestion(); 
+  
+  } else { 
+    renderResults(); }
 }
 
 window.onload = function() {
@@ -97,7 +110,7 @@ window.onload = function() {
   renderQuestion();
 }
 
-console.log([5]);
+console.log("[posNum][5]");
 console.log("correct");
 
 
